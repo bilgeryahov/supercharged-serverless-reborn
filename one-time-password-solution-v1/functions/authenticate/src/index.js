f0e3270006123ac78846924cf8a27182388b38c4
bilgeryahov <bayahov1@gmail.com>
2023-08-18 21:42:46 +0300

    Implementation of authenticate functionality.

--- one-time-password-solution-v1/functions/authenticate/src/index.js
+++ one-time-password-solution-v1/functions/authenticate/src/index.js
@@ -1,46 +1,77 @@
 const sdk = require("node-appwrite");
+const otplib = require('otplib');
+const crypto = require('node:crypto');
+const fetch = (...args) => import('node-fetch')
+  .then(({ default: fetch }) => fetch(...args));
 
-/*
-  'req' variable has:
-    'headers' - object with request headers
-    'payload' - request body data as a string
-    'variables' - object with function variables
-
-  'res' variable has:
-    'send(text, status)' - function to return text response. Status code defaults to 200
-    'json(obj, status)' - function to return JSON response. Status code defaults to 200
-
-  If an error is thrown, a response with code 500 will be returned.
-*/
+const db = '64de0c78978af6234921'; // db id
+const collection = 'otp-users'; // collection id
 
 module.exports = async function (req, res) {
   const client = new sdk.Client();
 
-  // You can remove services you don't use
-  const account = new sdk.Account(client);
-  const avatars = new sdk.Avatars(client);
   const database = new sdk.Databases(client);
-  const functions = new sdk.Functions(client);
-  const health = new sdk.Health(client);
-  const locale = new sdk.Locale(client);
-  const storage = new sdk.Storage(client);
-  const teams = new sdk.Teams(client);
   const users = new sdk.Users(client);
 
-  if (
-    !req.variables['APPWRITE_FUNCTION_ENDPOINT'] ||
-    !req.variables['APPWRITE_FUNCTION_API_KEY']
-  ) {
-    console.warn("Environment variables are not set. Function cannot use Appwrite SDK.");
-  } else {
-    client
-      .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
-      .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
-      .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
-      .setSelfSigned(true);
-  }
+  const { email, otp } = JSON.parse(req.payload);
+  const newPw = crypto.randomBytes(10).toString('hex');
+  let userId = null;
 
-  res.json({
-    areDevelopersAwesome: true,
-  });
+  client
+    .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
+    .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+    .setKey(req.variables['API_OTP_KEY']);
+
+  users.list(
+    [
+      sdk.Query.equal('email', email)
+    ]
+  )
+    .then(
+      (response) => {
+        if (response.total !== 1) {
+          return Promise.reject('Invalid authentication attempt!');
+        }
+
+        userId = response.users[0].$id;
+        return database.getDocument(db, collection, userId);
+      },
+      (error) => res.json(error)
+    )
+    .then(
+      (response) => {
+        if (otplib.authenticator.check(otp.toString(), response['user-secret'])) {
+          return Promise.resolve();
+        }
+        return Promise.reject('Unauthorized!');
+      },
+      (error) => res.json(error)
+    )
+    .then(
+      () => users.updatePassword(userId, newPw),
+      (error) => res.json(error)
+    )
+    .then(
+      () => fetch(
+        `${req.variables['APPWRITE_FUNCTION_ENDPOINT']}/account/sessions/email`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            "email": email,
+            "password": newPw
+          }),
+          headers: {
+            'X-Appwrite-Project': req.variables['APPWRITE_FUNCTION_PROJECT_ID'],
+            'Content-Type': 'application/json'
+          }
+        }
+      ),
+      (error) => res.json(error)
+    )
+    .then(
+      (response) => res.json({
+        headers: response.headers.raw()
+      }),
+      (error) => res.json(error)
+    );
 };
